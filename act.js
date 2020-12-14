3d104e1ac2ad274d9ae1ef6a132e23d4fa17fec3
unknown <git@github.com:burning1437/bagonggawa.git>
2020-12-14 13:27:53 +0800

    activities po namin sir

---
+++ act.js
@@ -0,0 +1,25 @@
+const section = document.querySelector('section');
+
+let para1 = document.createElement('p');
+let para2 = document.createElement('p');
+
+let motherInfo = 'The mother cats are called ';
+let kittenInfo;
+
+fetch('sample.json')
+.then(response => response.text())
+.then(text => displayCatInfo(text))
+
+function displayCatInfo(catString) {
+  let total = 0;
+  let male = 0;
+
+
+
+  para1.textContent = motherInfo;
+  para2.textContent = kittenInfo;
+}
+
+section.appendChild(para1);
+section.appendChild(para2);
+    
